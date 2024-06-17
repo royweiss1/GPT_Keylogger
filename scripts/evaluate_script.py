f98639764a2e5695f7e1b7729a4045bc01bd1fa1
royweiss <weissroy@post.bgu.ac.il>
2024-06-17 14:24:48 +0300

    changed results format

--- scripts/evaluate_script.py
+++ scripts/evaluate_script.py
@@ -7,10 +7,10 @@ from accelerate import Accelerator
 import torch
 from torch import nn
 import pickle
+import csv
 
-
-def format_floats(float_list):
-    return [float(f"{num:.3f}") for num in float_list]
+def format_floats(float_dict):
+    return {key: f"{value:.4f}" for key, value in float_dict.items()}
 
 
 def compute_metrics(model_sentence_transformers, reference_sentence, sentence_to_compare, evaluate_all_metrics):
@@ -36,10 +36,10 @@ def compute_metrics(model_sentence_transformers, reference_sentence, sentence_to
     ref_set, comp_set = set(ref.split()), set(comp.split())
     jac = float(len(ref_set & comp_set)) / len(ref_set | comp_set)
         
-    return format_floats([cosine_score[0], rouge_1, rouge_L, ed, jac])
+    return format_floats({"Cosine":cosine_score[0], "Rouge1":rouge_1, "RougeL":rouge_L, "Edit Distance":ed, "Jaccard":jac})
 
 
-def calculate_scores(csv_file_path, output_pickle_path, evaluate_all_metrics, start_idx=0, end_idx=None, checkpoint_interval=500):
+def calculate_scores(csv_file_path, output_csv_path, evaluate_all_metrics, start_idx=0, end_idx=None, checkpoint_interval=500):
     # Load the combined CSV file
     accelerator = Accelerator(cpu=False)
     torch.cuda.empty_cache()
@@ -55,21 +55,21 @@ def calculate_scores(csv_file_path, output_pickle_path, evaluate_all_metrics, st
         end_idx = len(df)
 
     # Initialize a list to hold all other scores
-    all_other_scores = []
+    csv_rows = []
 
-    for idx in tqdm(range(start_idx, end_idx), desc="Processing rows: "):
-        row_other_scores = []
+
+    for paragraph_idx in tqdm(range(start_idx, end_idx), desc="Processing rows: "):
         
         reference_para = []
         generated_para = []
         
-        for i in range(45): # should be max 45 sentences
-            sentence_col = f"Sentence_{i}"
-            generated_col = f"Generated_{i}"
+        for sentence_idx in range(45): # should be max 45 sentences
+            sentence_col = f"Sentence_{sentence_idx}"
+            generated_col = f"Generated_{sentence_idx}"
 
             if sentence_col in df.columns and generated_col in df.columns:
-                reference_sentence = df.at[idx, sentence_col]
-                generated_sentence = df.at[idx, generated_col]
+                reference_sentence = df.at[paragraph_idx, sentence_col]
+                generated_sentence = df.at[paragraph_idx, generated_col]
 
                 if str(reference_sentence) != "nan" and str(generated_sentence) != "nan":
                     reference_para.append(reference_sentence)
@@ -79,30 +79,40 @@ def calculate_scores(csv_file_path, output_pickle_path, evaluate_all_metrics, st
                     gen_combined = "".join(generated_para)
 
                     scores = compute_metrics(model_sentence_transformers, ref_combined, gen_combined, evaluate_all_metrics)
-                    row_other_scores.append(scores)
-        
-        all_other_scores.append(row_other_scores)
+                    row = {
+                        "Paragraph": paragraph_idx,
+                        "Sentence": sentence_idx,
+                        **scores
+                    }
+                    csv_rows.append(row)
+
 
         # Save the checkpoint
-        if (idx + 1) % checkpoint_interval == 0:
-            with open(output_pickle_path, 'wb') as checkpoint_file:
-                pickle.dump(all_other_scores, checkpoint_file)
+        if (paragraph_idx + 1) % checkpoint_interval == 0:
+            fieldnames = csv_rows[0].keys()
+            with open(output_csv_path, 'w', newline='') as output_file:
+                writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(csv_rows)
 
-    # Save the final results to the pickle file
-    with open(output_pickle_path, 'wb') as output_file:
-        pickle.dump(all_other_scores, output_file)
 
-    print(f"Results saved to {output_pickle_path}")
+    fieldnames = csv_rows[0].keys()
+    with open(output_csv_path, 'w', newline='') as output_file:
+        writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(csv_rows)
+
+    print(f"Results saved to {output_csv_path}")
 
 
 
 # here you can run statistical analysis on the scores
-def read_and_print_pickle(pickle_file_path):
-    with open(pickle_file_path, 'rb') as file:
-        data = pickle.load(file)
-    
-    # Print the contents of the pickle file
-    firsts = [row[0] for row in data]
+def read_and_print_csv(csv_file_path):
+    # Load the CSV file
+    df = pd.read_csv(csv_file_path)
+
+    # take those who Sentence=0
+    firsts = df[df["Sentence"] == 0]["Cosine"].values
 
     print(f"Attack Success Rate, first Sentences: {sum(1 for number in firsts if number[0] > 0.5) / len(firsts) * 100:.2f}%")
     print(f"Precentege of almost Identical Decyphering, first Sentences: {sum(1 for number in firsts if number[0] > 0.9) / len(firsts) * 100:.2f}%")
@@ -111,6 +121,6 @@ def read_and_print_pickle(pickle_file_path):
 
 def main(generated_output_path: str, generated_metrics_path: str, evaluate_all_metrics: bool):
     calculate_scores(generated_output_path, generated_metrics_path, evaluate_all_metrics)
-    read_and_print_pickle(generated_metrics_path)
+    read_and_print_csv(generated_metrics_path)
 
 
